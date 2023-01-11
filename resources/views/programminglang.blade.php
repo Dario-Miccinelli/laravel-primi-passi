85fa41e3ff673d1d238094fb7e885c82ff79906e
Dario-Miccinelli <poduzzo@hotmail.com>
2023-01-11 13:13:38 +0100

    exercise finish

---
+++ resources/views/programminglang.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Programming Lang</title>
+</head>
+<body>
+<h1>Programming Lang: Laravel!</h1>
+<nav>
+  <a class="" href="/">Home</a>
+  <a class="" href="/aboutus">About us</a>
+  <a class="" href="/programminglang">Programming lang</a>
+</nav>
+</body>
+</html>
+
+<style>
+
+
+* {
+    background-color: black;
+    color: white;
+}
+    a,h1,h3 {
+        font-size: 2rem;
+        text-decoration: none;
+        padding: 1rem;
+    }
+</style>
